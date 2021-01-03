9c83d04156c7acac3bfe4393aea4301f92276c8a
mariaaldis <maria.aldisg@gmail.com>
2021-01-03 22:52:30 +0200

    Sidenav added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import Header from "./shared/components/Header";
+import SideNav from './shared/components/SideNav';
 
 import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Dashboard from './dashboard/pages/Dashboard';
 import WidgetBuilder from './widget-builder/pages/WidgetBuilder';
@@ -17,46 +17,30 @@ import Settings from './settings/pages/Settings';
 function App() {
   return (
     <Router>
-      <div>
-        <Header></Header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/widget-builder">Widget Builder</Link>
-            </li>
-            <li>
-              <Link to="/departments">Departments</Link>
-            </li>
-            <li>
-              <Link to="/co-workers">Co-Workers</Link>
-            </li>
-            <li>
-              <Link to="/settings">Settings</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="main-container">
+        <SideNav className="side-nav"></SideNav>
 
+        <div className="main">
+          <Header></Header>
 
-        <Switch>
-          <Route path="/" exact>
-            <Dashboard />
-          </Route>
-          <Route path="/widget-builder">
-            <WidgetBuilder />
-          </Route>
-          <Route path="/departments">
-            <Departments />
-          </Route>
-          <Route path="/co-workers">
-            <CoWorkers />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/widget-builder">
+              <WidgetBuilder />
+            </Route>
+            <Route path="/departments">
+              <Departments />
+            </Route>
+            <Route path="/co-workers">
+              <CoWorkers />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
--- client/src/shared/components/Header.js
+++ client/src/shared/components/Header.js
@@ -8,20 +8,20 @@ const Header = props => {
     return (
         <header>
             <svg width="94.5" height="32" viewBox="0 0 94.5 32">
-                <g id="Group_441" data-name="Group 441" transform="translate(-266 -12)">
-                    <g id="Group_380" data-name="Group 380" transform="translate(188 1255.021)">
-                    <g id="Rectangle_113" data-name="Rectangle 113" transform="translate(78 -1243.021)" fill="none" stroke="#ffa000" stroke-width="2">
+                <g id="Group_441" transform="translate(-266 -12)">
+                    <g id="Group_380" transform="translate(188 1255.021)">
+                    <g id="Rectangle_113" transform="translate(78 -1243.021)" fill="none" stroke="#ffa000" strokeWidth="2">
                         <rect width="32" height="32" rx="2" stroke="none"/>
                         <rect x="1" y="1" width="30" height="30" rx="1" fill="none"/>
                     </g>
-                    <text id="W" transform="translate(85 -1220.021)" fill="#ffa000" font-size="18" font-family="SegoeUI-Semibold, Segoe UI" font-weight="600"><tspan x="0" y="0">W</tspan></text>
-                    <line id="Line_25" data-name="Line 25" x2="56" transform="translate(115.5 -1212.021)" fill="none" stroke="#f5a40a" stroke-linecap="round" stroke-width="2"/>
+                    <text id="W" transform="translate(85 -1220.021)" fill="#ffa000" fontSize="18" fontFamily="SegoeUI-Semibold, Segoe UI" fontWeight="600"><tspan x="0" y="0">W</tspan></text>
+                    <line id="Line_25" x2="56" transform="translate(115.5 -1212.021)" fill="none" stroke="#f5a40a" strokeLinecap="round" strokeWidth="2"/>
                     </g>
-                    <text id="Builder" transform="translate(302 34)" fill="#ffa000" font-size="18" font-family="SegoeUI-Semibold, Segoe UI" font-weight="600"><tspan x="0" y="0">Builder</tspan></text>
+                    <text id="Builder" transform="translate(302 34)" fill="#ffa000" fontSize="18" fontFamily="SegoeUI-Semibold, Segoe UI" fontWeight="600"><tspan x="0" y="0">Builder</tspan></text>
                 </g>
             </svg>
            
-             <div class="container">
+             <div className="container">
                 <Badge>
                     <svg width="16" height="19.5" viewBox="0 0 16 19.5">
                         <path id="notifications-24px" d="M12,22a2.006,2.006,0,0,0,2-2H10A2,2,0,0,0,12,22Zm6-6V11c0-3.07-1.64-5.64-4.5-6.32V4a1.5,1.5,0,0,0-3,0v.68C7.63,5.36,6,7.92,6,11v5L4,18v1H20V18Z" transform="translate(-4 -2.5)" />
