65db5e97566b855d2668f07b819263b9bd16a5b1
mariaaldis <maria.aldisg@gmail.com>
2021-01-03 16:38:00 +0200

    Header created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./shared/components/Header";
 
 function App() {
   return (
     <div className="App">
+        <Header></Header>
         <p> Widget Builder </p>
     </div>
   );
